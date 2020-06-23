084f7f7e43a64542ccea14d6abf96f21fbd858ef
Yash Mehta <yashsmehta95@gmail.com>
2020-06-23 20:55:07 +0100

    modified LM extractor to use AlBERT + embedding storing

--- LM_extractor.py
+++ LM_extractor.py
@@ -10,13 +10,7 @@ import torch
 from torch.utils.data import DataLoader, Dataset
 from transformers import *
 
-import utils.gen_utils as utils
-from utils.data_utils import MyMapDataset
-
 start = time.time()
-# argument extractor
-dataset_type, token_length, datafile, batch_size, embed, op_dir, mode, embed_mode = utils.parse_args_extractor()
-print('{} : {} : {} : {} : {}'.format(dataset_type, embed, token_length, mode, embed_mode))
 
 if torch.cuda.is_available():
     DEVICE = torch.device("cuda")
@@ -28,38 +22,21 @@ else:
     DEVICE = torch.device('cpu')
     print('running on cpu')
 
-# * Model          | Tokenizer          | Pretrained weights shortcut
-# MODEL=(DistilBertModel, DistilBertTokenizer, 'distilbert-base-uncased')
-if (embed == 'bert-base'):
-    n_hl = 12
-    hidden_dim = 768
-    MODEL = (BertModel, BertTokenizer, 'bert-base-uncased')
+n_hl = 12
+batch_size = 32
+hidden_dim = 768
+max_token_length = 512
+entity_class = 'countries'
+entity_class_id = 1
+datafile = 'data/' + entity_class + '.txt'
+op_dir = 'data/pkl_data/'
 
-elif (embed == 'bert-large'):
-    n_hl = 24
-    hidden_dim = 1024
-    MODEL = (BertModel, BertTokenizer, 'bert-large-uncased')
+model_class, tokenizer_class, pretrained_weights = AlbertModel, AlbertTokenizer, 'albert-base-v2'
 
-elif (embed == 'albert-base'):
-    n_hl = 12
-    hidden_dim = 768
-    MODEL = (AlbertModel, AlbertTokenizer, 'albert-base-v2')
-
-elif (embed == 'albert-large'):
-    n_hl = 24
-    hidden_dim = 1024
-    MODEL = (AlbertModel, AlbertTokenizer, 'albert-large-v2')
-
-model_class, tokenizer_class, pretrained_weights = MODEL
-
-# load the LM model and tokenizer from the HuggingFace Transformeres library
 model = model_class.from_pretrained(pretrained_weights, output_hidden_states=True)  # output_attentions=False
 tokenizer = tokenizer_class.from_pretrained(pretrained_weights, do_lower_case=True)
 
-# create a class which can be passed to the pyTorch dataloader. responsible for returning tokenized and encoded values of the Essays dataset
-# this class will have __getitem__(self,idx) function which will return input_ids and target values
-# currently it is just returning the targets for the 'OPN' trait - need to generalize this
-map_dataset = MyMapDataset(dataset_type, datafile, tokenizer, token_length, DEVICE, mode)
+map_dataset = MyMapDataset(datafile, tokenizer, max_token_length, DEVICE, entity_class_id)
 
 data_loader = DataLoader(dataset=map_dataset,
                          batch_size=batch_size,
@@ -69,65 +46,27 @@ data_loader = DataLoader(dataset=map_dataset,
 if (DEVICE == torch.device("cuda")):
     model = model.cuda()
     # model.parameters() returns a generator obj
-    # print('model loaded to gpu? ', next(model.parameters()).is_cuda)
     print('\ngpu mem alloc: ', round(torch.cuda.memory_allocated() * 1e-9, 2), ' GB')
 
 print('starting to extract LM embeddings...')
 
 hidden_features = []
 all_targets = []
-all_author_ids = []
 
-for author_ids, input_ids, targets in data_loader:
+for input_ids, targets in data_loader:
     with torch.no_grad():
         all_targets.append(targets.cpu().numpy())
-        all_author_ids.append(author_ids.cpu().numpy())
-        
-        if (mode == 'docbert'):
-            # print(input_ids.shape)
-            tmphidden_features = []
-            input_ids = input_ids.permute(1,0,2)
+        tmp = []
+        bert_output = model(input_ids)
+    
+        for ii in range(n_hl):
+            tmp.append((bert_output[2][ii + 1].cpu().numpy()).mean(axis=1))
             
-            for jj in range(input_ids.shape[0]):
-                tmp = []
-                if(input_ids[jj][0][0] == 0):
-                    break
-
-                bert_output = model(input_ids[jj])
-                for ii in range(n_hl):
-                    if(embed_mode == 'mean'):
-                        tmp.append((bert_output[2][ii + 1].cpu().numpy()).mean(axis=1))
-                    elif(embed_mode == 'cls'):
-                        tmp.append(bert_output[2][ii + 1][:, 0, :].cpu().numpy())
-
-                tmphidden_features.append(tmp)
-            
-            
-            tmphidden_features = np.array(tmphidden_features)
-            hidden_features.append(tmphidden_features.mean(axis=0))
-
-        else:
-            tmp = []
-            bert_output = model(input_ids)
-            # bert_output[2](this id gives all BERT outputs)[ii+1](which BERT layer)[:,0,:](taking the <CLS> output)
-        
-            for ii in range(n_hl):
-                if(embed_mode == 'cls'):
-                    tmp.append(bert_output[2][ii + 1][:, 0, :].cpu().numpy())
-                elif(embed_mode == 'mean'):
-                    tmp.append((bert_output[2][ii + 1].cpu().numpy()).mean(axis=1))
-                
-            hidden_features.append(np.array(tmp))
-        
-# storing the embeddings into a pickle file
-
-file = open(op_dir + dataset_type + '-' + embed + '-' +embed_mode + '-' + mode + '.pkl', 'wb')
-pickle.dump(zip(all_author_ids, hidden_features, all_targets), file)
+        hidden_features.append(np.array(tmp))
+    
+file = open(op_dir + '-' + entity_class + 'embedings.pkl', 'wb')
+pickle.dump(zip(hidden_features, all_targets), file)
 file.close()
 
 print(timedelta(seconds=int(time.time() - start)), end=' ')
-print('extracting embeddings for {} dataset: DONE!'.format(dataset_type))
-
-# input_ids = input_ids.cpu().numpy()
-# subdoc_input_ids = torch.from_numpy(subdoc_input_ids).long().to(DEVICE)
-                
+print('embeddings extraction for {} - DONE!'.format(entity_class))                
--- scraper.py
+++ scraper.py
@@ -1,39 +1,57 @@
 import urllib
 import requests
 import re
+import pandas as pd
+import wikipediaapi as wp
+
 from bs4 import BeautifulSoup
 
-query = "London" + " wikipedia"
-query = query.replace(' ', '+')
-URL = f"https://google.com/search?q={query}"
+def get_wiki_para(query):
+    query += " wikipedia"
+    query = query.replace(' ', '+')
+    URL = f"https://google.com/search?q={query}"
 
-#google returns different results for desktop and mobile queries. Specifying desktop here!
-USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
+    #google returns different results for desktop and mobile queries. Specifying desktop here!
+    USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
 
-headers = {"user-agent" : USER_AGENT}
-resp = requests.get(URL, headers=headers)
+    headers = {"user-agent" : USER_AGENT}
+    resp = requests.get(URL, headers=headers)
 
-if resp.status_code == 200:
-    soup = BeautifulSoup(resp.content, "html.parser")
+    if resp.status_code == 200:
+        soup = BeautifulSoup(resp.content, "html.parser")
 
-# all_results = []
+    for g in soup.find_all('div', class_='r'):
+        anchors = g.find_all('a')
+        if anchors:
+            URL = anchors[0]['href']
+            title = g.find('h3').text
+            match = re.search('wikipedia', URL)
+            if(match):
+                break
 
-for g in soup.find_all('div', class_='r'):
-    anchors = g.find_all('a')
-    if anchors:
-        URL = anchors[0]['href']
-        title = g.find('h3').text
-        # item = {
-        #     "title": title,
-        #     "link": link
-        # }
-        # all_results.append(item)
-        match = re.search('wikipedia', URL)
-        if(match):
-            break
+    page = requests.get(URL)
+    wiki = BeautifulSoup(page.text, 'html.parser')
 
-page = requests.get(URL)
-wiki = BeautifulSoup(page.text, 'html.parser')
+    for i in wiki.select('p'):
+        return i.getText()
 
-for i in wiki.select('p'):
-    print(i.getText())
+
+def print_categories(page):
+    categories = page.categories
+    for title in sorted(categories.keys()):
+        print("%s: %s" % (title, categories[title]))
+
+def get_cities_list():
+    URL = 'https://en.wikipedia.org/wiki/List_of_largest_cities'
+    res = requests.get(URL).text
+    soup = BeautifulSoup(res,'lxml')
+    
+    city_table = soup.find('table',{'class':'sortable wikitable mw-datatable'})
+
+    city_names = []
+    
+    for row in city_table.find_all('tr')[1:]:
+        state_cell = row.find_all('a')[0]  
+        states.append(state_cell.text)
+    print(states)
+
